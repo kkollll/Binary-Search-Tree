ea1d877e7da4b46d83d7a7a9e45281c9ef3f05dd
Wr2020 <comin_008@163.com>
2020-06-03 12:40:36 +0800

    非递归add 二叉树

--- src/BST.java
+++ src/BST.java
@@ -27,4 +27,56 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+        } else {
+            Node cur = root;
+            while (true) {
+                if (e.compareTo(cur.e) == 0 ) {
+                    return;
+                }
+                if (e.compareTo(cur.e) > 0 ) {
+                    if (cur.right == null) {
+                        cur.right = new Node(e);
+                        break;
+                    } else {
+                        cur = cur.right;
+                    }
+                }
+                if (e.compareTo(cur.e) < 0) {
+                    if (cur.left == null) {
+                        cur.left = new Node(e);
+                        break;
+                    } else {
+                        cur = cur.left;
+                    }
+                }
+            }
+        }
+        size++;
+    }
+
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        bst.add(7);
+        bst.add(4);
+        bst.add(2);
+        bst.add(3);
+        bst.add(10);
+        bst.add(8);
+        bst.add(11);
+
+        System.out.println(bst.root.e);
+        System.out.println(bst.root.left.e);
+        System.out.println(bst.root.left.left.e);
+        System.out.println(bst.root.left.left.right.e);
+        System.out.println(bst.root.e);
+        System.out.println(bst.root.right.e);
+        System.out.println(bst.root.right.left.e);
+        System.out.println(bst.root.right.right.e);
+        System.out.println(bst.getSize());
+    }
 }
